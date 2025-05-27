aefabad3c6f44840d30c36cdbe37e8d2e61c1af1
VladFirsov <firsovvs@answer-42.ru>
2025-05-27 21:10:35 +0500

    исправляю сборку

--- src/index.ts
+++ src/index.ts
@@ -13,8 +13,3 @@ export * from './utils'
 // Оставляем этот экспорт, он будет работать для пользователей,
 // которые импортируют React напрямую
 export * from './react'
-
-// Информационное сообщение для дебаггинга
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Synapse state management library loaded ✨')
-}
