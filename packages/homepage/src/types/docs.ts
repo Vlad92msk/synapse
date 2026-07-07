756b24d0634a2b194d0a843543d5d9ebfd3815f5
VladFirsov <firsovvs@answer-42.ru>
2026-07-07 21:51:29 +0500

    .

--- packages/homepage/src/types/docs.ts
+++ packages/homepage/src/types/docs.ts
@@ -1,5 +1,5 @@
 // Auto-generated types for structured documentation
-// Generated at: 2026-07-07T16:49:12.877Z
+// Generated at: 2026-07-07T16:51:20.455Z
 // Master locale: en
 
 export type Locale = 'en' | 'ru'
